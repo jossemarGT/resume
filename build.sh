6ee07fe93294ce66313aca6d374c8e8d6a592d70
jossemargt <jossemargt@gmail.com>
2017-02-20 22:43:50 -0600

    Update styles, layout and build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo  ==============================================================================
+set -ex
+
 echo  :: Generating Resume Page
-docker run -v `pwd`:/source --rm jagregory/pandoc --smart --wrap=none --normalize \
-        --section-divs --no-highlight \
+docker run -v "`pwd`":/source --rm jagregory/pandoc \
+        --smart --wrap=none --normalize --section-divs --no-highlight \
         --from       markdown_github-hard_line_breaks+yaml_metadata_block \
         --to         html5 \
         --template   docs/resume.template \
