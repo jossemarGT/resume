d4320eaa1ec7bcffecc5be00e81b16dcbbf190d3
jossemargt <jossemargt@gmail.com>
2017-12-23 22:19:36 -0600

    Generate PDF file as release artifact
    What else could be taken as a releasable artifact for markdown or html
    source files than PDF files? Each iteration now is able to have an
    "snapshot" of it by generating PDF "binaries".
    
    Regarding the build tools, I picked Wkhtmltopdf over Pandoc, because
    I found the latter quite limited for html to pdf conversion. By the way
    madnight/docker-alpine-wkhtmltopdf Alpine based Docker image is a
    great alternative in size ~105MB and it already comes patched to
    not rely on a X11 server running inside the container.

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-set -o pipefail
+set -eo pipefail
 
 BUILD_DATE=$(date +%B\ %d,\ %Y)
 TITLE=$(sed -n -e "s/^title:[[:space:]]*'\([^']\+\).*'/\1/p" docs/configuration.yaml)
 
-# Generates HTML page on docs directory using RESUME.md file and pandoc docker
+# Generates HTML page inside docs/ directory using RESUME.md file and Pandoc Docker
 # container
 generate_page () {
   echo  ":: Generating ${TITLE} Page"
   docker run -v "$(pwd)":/tmp/source -w /tmp/source --rm portown/alpine-pandoc pandoc \
         --verbose --fail-if-warnings --standalone --wrap=none --section-divs --no-highlight \
-        --from      markdown_github+yaml_metadata_block+auto_identifiers+smart-hard_line_breaks \
-        --to        html5 \
-        --template  docs/resume.template \
-        --output    docs/index.html \
+        --from markdown_github+yaml_metadata_block+auto_identifiers+smart-hard_line_breaks \
+        --to html5 --template docs/resume.template --output docs/index.html \
         ./RESUME.md docs/configuration.yaml
 }
 
@@ -22,6 +20,18 @@ bump_date () {
   sed -i "s/date:.*/date:   ${BUILD_DATE}/" docs/configuration.yaml
 }
 
+# Generates PDF file inside dist/ directory using docs/index.html file and
+# wkhtmltopdf Docker container
+generate_pdf () {
+  [ -d dist ] || mkdir dist
+  echo  ":: Generating ${TITLE} PDF"
+  docker run -v "$(pwd)":/tmp/source -w /tmp/source --rm madnight/docker-alpine-wkhtmltopdf \
+        --title "${TITLE}" --page-size Letter --no-background \
+        --print-media-type --viewport-size 520px \
+        docs/index.html dist/jossemargt-resume.pdf
+}
+
 # Using the functions defined above
 bump_date
 generate_page
+generate_pdf
