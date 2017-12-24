f3606c0074c0a48b75272d4506a4dd4773639c91
jossemargt <jossemargt@gmail.com>
2017-12-23 19:50:13 -0600

    Update date in pandoc configuration file for each page build
    Each time that build.sh script is called it modifies the
    docs/configuration.yml file with the current date before generating
    the html content, in that way the revision and DC Term date metadata
    values are updated accordly.

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
-set -eo pipefail
+#!/bin/bash
+set -o pipefail
 
-echo  :: Generating Resume Page
-docker run -v "`pwd`":/source --rm jagregory/pandoc \
+BUILD_DATE=$(date +%B\ %d,\ %Y)
+TITLE=$(sed -n -e "s/^title:[[:space:]]*'\([^']\+\).*'/\1/p" docs/configuration.yaml)
+
+# Generates HTML page on docs directory using RESUME.md file and pandoc docker
+# container
+generate_page () {
+  echo  ":: Generating ${TITLE} Page"
+  docker run -v "$(pwd)":/source --rm jagregory/pandoc \
         --smart --wrap=none --normalize --section-divs --no-highlight \
         --from       markdown_github-hard_line_breaks+yaml_metadata_block \
         --to         html5 \
         --template   docs/resume.template \
         --output     docs/index.html \
         ./RESUME.md  docs/configuration.yaml
+}
+
+# Bumps last modified date in pandoc template configuration
+bump_date () {
+  sed -i "s/date:.*/date:   ${BUILD_DATE}/" docs/configuration.yaml
+}
+
+# Using above defined functions
+bump_date
+generate_page
