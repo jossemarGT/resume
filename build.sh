05e8f41549dc1711ae2e913ac89526dcf9cc3983
jossemargt <jossemargt@gmail.com>
2021-09-11 18:06:33 -0600

    Migrate build process to GH actions
    Woops, somehow this repository stagnated on CircleCI. I have to
    say GitHub Actions DSL learning curve is a little bit more diffi-
    cult when compared with the CircleCI one, but that is a little
    price to pay in order to more flexibility and composite-ability.

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ set -euo pipefail
 GOAL=${1-default}
 SRC_PATH=${SRC_PATH-$(pwd)}
 BUILD_DATE=$(date +%B\ %d,\ %Y)
-TITLE=$(sed -n -e "s/^title:[[:space:]]*'\([^']\+\).*'/\1/p" docs/configuration.yaml)
 
 ##
 # Generates HTML page inside docs/ directory using RESUME.md file and Pandoc Docker
 # container
 generate_page () {
-  echo  ":: Generating ${TITLE} Page"
+  echo  ":: Generating Page"
   docker run -v "${SRC_PATH}":/tmp/source -w /tmp/source --rm portown/alpine-pandoc pandoc \
         --verbose --fail-if-warnings --standalone --wrap=none --section-divs --no-highlight \
         --from markdown_github+yaml_metadata_block+auto_identifiers+smart-hard_line_breaks \
@@ -31,9 +30,10 @@ bump_date () {
 # wkhtmltopdf Docker container
 generate_pdf () {
   [ -d dist ] || mkdir dist
-  echo  ":: Generating ${TITLE} PDF"
+  echo  ":: Generating PDF"
   docker run -v "${SRC_PATH}":/tmp/source -w /tmp/source --rm madnight/docker-alpine-wkhtmltopdf \
-        --title "${TITLE}" --page-size Letter --no-background --print-media-type \
+        --page-size Letter --no-background --print-media-type \
+        --enable-local-file-access -T 20 -R 13 -B 13 -L 13 \
         --user-style-sheet docs/stylesheets/wkhtmltopdf.css \
         docs/index.html dist/jossemargt-resume.pdf
 }
@@ -45,8 +45,8 @@ generate_pdf () {
 git_setup () {
   if [[ -z $(git config --local user.name) ]]; then
     echo ':: Setup CI git user and email'
-    git config --local user.name 'Deployment Bot'
-    git config --local user.email 'deploy-bot@jossemargt.com'
+    git config --local user.name 'CI Bot'
+    git config --local user.email 'no-reply@jossemargt.com'
   fi
 }
 
@@ -83,11 +83,13 @@ git_publish () {
 ##
 # Using the functions defined above
 case "$GOAL" in
-  ci-git-setup)
+  ci-setup)
+    [ -d dist ] || mkdir dist
     git_setup
-    git_tag_bump
+    bump_date
     ;;
   ci-git-push)
+    git_tag_bump
     git_publish # A fenced git push
     ;;
   default)
